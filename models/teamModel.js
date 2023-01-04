814f1a91adb629d786fa23cdd9ad4f94510d95a6
arnit-coder <21ucs029@lnmiit.ac.in>
2023-01-04 14:00:06 +0530

    emails added in team model

--- models/teamModel.js
+++ models/teamModel.js
@@ -7,7 +7,13 @@ const teamSchema = new mongoose.Schema({
     email1 : String,  //Team leader email
     email2 : String,
     email3 : String,
-    email4 : String, 
+    email4 : String,
+    email5 : String,
+    email6 : String,
+    email7 : String,
+    email8 : String,
+    email9 : String,
+    email10 : String, 
 })
 
 const Team = mongoose.model("Team", teamSchema)
